0c6b1b75d7f24d159934416ad0943e59d73cd124
Kazuki1131 <onetsubyo@gmail.com>
2022-03-26 19:27:34 +0900

    add function updateStatus

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,7 +1,7 @@
 import { ItemStatus } from './item-status.enum';
 import { Item } from 'src/items/item.model';
 import { ItemsService } from './items.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
@@ -32,5 +32,10 @@ export class ItemsController {
       status: ItemStatus.ON_SALE,
     };
     return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
   }
 }
