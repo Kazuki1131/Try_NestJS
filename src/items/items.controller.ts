02dcc577b5a51c913dbec4b2009feefa0ec0a35c
Kazuki1131 <onetsubyo@gmail.com>
2022-03-26 17:57:29 +0900

    add feature create

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,5 +1,7 @@
+import { ItemStatus } from './item-status.enum';
+import { Item } from 'src/items/item.model';
 import { ItemsService } from './items.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
@@ -8,5 +10,22 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { Item } from 'src/items/item.model';
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
+
   findAll() {
     return 'This is ItemsService';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
